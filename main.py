b1345c8a4ffd20ef7eb7245d39f26cbfe899a129
57412698 <gagarinkomar@users.noreply.github.com>
2019-12-15 03:17:29 +0300

    Создание terminate()(Экстренное завершение), draw_text() и доделана screenintro()

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ import os
 WIDTH, HEIGHT = 600, 800
 FPS = 60
 
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('GalaxyShooter')
+clock = pygame.time.Clock()
+
+
+def terminate():
+    pygame.quit()
+
 
 def load_image(name, color_key=None):
-    fullname = os.path.join('data/images', name)
+    fullname = os.path.join('data', 'images', name)
     try:
         image = pygame.image.load(fullname).convert()
     except pygame.error as message:
@@ -24,7 +34,7 @@ def load_image(name, color_key=None):
 
 
 def load_sound(name):
-    fullname = os.path.join('data/sounds', name)
+    fullname = os.path.join('data', 'sounds', name)
     try:
         image = pygame.image.load(fullname).convert()
     except pygame.error as message:
@@ -33,17 +43,50 @@ def load_sound(name):
     return image
 
 
-def screenIntro():
-    pass
+def draw_text(screen, text, size, x, y, color):
+    font = pygame.font.Font(None, size)
+    string_rendered = font.render(text, 1, color)
+    intro_rect = string_rendered.get_rect()
+    intro_rect.midtop = (x, y)
+    screen.blit(string_rendered, intro_rect)
+
+
+def screenIntro(screen):
+    background = load_image('background_intro.png')
+    color = pygame.Color('White')
+    hsv = color.hsva
+    color.hsva = (hsv[0], hsv[1], 0, hsv[3])
+    valuechanging = 1
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+
+        screen.blit(background, (0, 0))
+        draw_text(screen, 'GK', 50, WIDTH // 2, HEIGHT // 2 - 100, color)
+        draw_text(screen, 'Production', 50, WIDTH // 2, HEIGHT // 2 - 50,
+                  color)
+        draw_text(screen, 'Present', 50, WIDTH // 2, HEIGHT // 2, color)
+
+        hsv = color.hsva
+
+        if valuechanging == 1 and (hsv[2] + valuechanging) > 60:
+            valuechanging = -1
+        elif valuechanging == -1 and (hsv[2] + valuechanging) < 1:
+            terminate()
+
+        color.hsva = (hsv[0], hsv[1], hsv[2] + valuechanging, hsv[3])
+
+        pygame.display.flip()
+
+        clock.tick(20)
+
 
 
 
 def main():
-    pygame.init()
-    pygame.mixer.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption('GalaxyShooter')
-    clock = pygame.time.Clock()
+    screenIntro(screen)
 
-    pass
+main()
 
