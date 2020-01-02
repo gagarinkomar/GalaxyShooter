deb02ba1f8352b14bfecd42fab3722c7ed7756b1
57412698 <gagarinkomar@users.noreply.github.com>
2020-01-02 05:41:37 +0300

    Добавлен класс Button и функция создания главного меню(screenMainmenu)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import sys
 
 
 WIDTH, HEIGHT = 600, 800
@@ -14,6 +15,7 @@ clock = pygame.time.Clock()
 
 def terminate():
     pygame.quit()
+    sys.exit()
 
 
 def load_image(name, color_key=None):
@@ -35,12 +37,7 @@ def load_image(name, color_key=None):
 
 def load_sound(name):
     fullname = os.path.join('data', 'sounds', name)
-    try:
-        image = pygame.image.load(fullname).convert()
-    except pygame.error as message:
-        print('Cannot load sound:', name)
-        raise SystemExit(message)
-    return image
+    pass
 
 
 def draw_text(screen, text, size, x, y, color):
@@ -51,7 +48,7 @@ def draw_text(screen, text, size, x, y, color):
     screen.blit(string_rendered, intro_rect)
 
 
-def screenIntro(screen):
+def screenIntro():
     background = load_image('background_intro.png')
     color = pygame.Color('White')
     hsv = color.hsva
@@ -74,7 +71,7 @@ def screenIntro(screen):
         if valuechanging == 1 and (hsv[2] + valuechanging) > 60:
             valuechanging = -1
         elif valuechanging == -1 and (hsv[2] + valuechanging) < 1:
-            terminate()
+            break
 
         color.hsva = (hsv[0], hsv[1], hsv[2] + valuechanging, hsv[3])
 
@@ -83,10 +80,76 @@ def screenIntro(screen):
         clock.tick(20)
 
 
+def screenMainmenu():
+    background = load_image('background_mainmenu.png')
+    buttonFunctions = {1: screenChooseLevel}
+    all_sprites = pygame.sprite.Group()
+    buttonPlay = Button(all_sprites, (pygame.Color('#00BFFF'), pygame.Color('#87CEFA')), (200, 100), (300, 400), ('Выбрать уровень', 30, pygame.Color('White')))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if buttonPlay.isPressed():
+                    pressedButton = 1
+                    running = False
+
+        screen.blit(background, (0, 0))
+
+        all_sprites.draw(screen)
+        all_sprites.update()
+
+        pygame.display.flip()
+
+        clock.tick(FPS)
+
+    buttonFunctions[pressedButton](123)
+
+
+def screenChooseLevel(a):
+    background = load_image('background_mainmenu.png')
+    all_sprites = pygame.sprite.Group()
+
+    pass
+
+
+
+
+
+
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, spriteGroup, colors, size, posCenter, textInfo):
+        super().__init__(spriteGroup)
+        self.colors = colors
+        self.size = size
+        self.posCenter = posCenter
+        self.textInfo = textInfo
+
+
+        self.image = pygame.Surface(self.size)
+        self.image.fill(self.colors[0])
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.posCenter[0]
+        self.rect.centery = self.posCenter[1]
+        draw_text(self.image, self.textInfo[0], self.textInfo[1], self.rect.width // 2, self.rect.height // 2 - self.textInfo[1] // 4, self.textInfo[2])
+
+    def isPressed(self):
+        return self.rect.collidepoint(pygame.mouse.get_pos())
+
+    def update(self):
+        if self.isPressed():
+            self.image.fill(self.colors[1])
+        else:
+            self.image.fill(self.colors[0])
+        draw_text(self.image, self.textInfo[0], self.textInfo[1], self.rect.width // 2, self.rect.height // 2 - self.textInfo[1] // 4, self.textInfo[2])
 
 
 def main():
-    screenIntro(screen)
+    #screenIntro()
+    screenMainmenu()
 
 main()
 
